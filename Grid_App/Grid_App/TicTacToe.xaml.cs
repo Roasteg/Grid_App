e0f4a62eed3835ba00469acd53a94d18ab937239
Rostislav <Rostislav@DESKTOP-0GKVI9I>
2020-09-29 18:26:54 +0300

    Draw added. Text alignment fixed.

--- Grid_App/Grid_App/TicTacToe.xaml.cs
+++ Grid_App/Grid_App/TicTacToe.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Markup;
 using Xamarin.Forms.Xaml;
 
@@ -26,7 +28,7 @@ namespace Grid_App
             void Reset()
             {
                 Grid grid = new Grid();
-                for (int g = 0; g < 4; g++)
+                for (int g = 0; g < 3; g++)
                 {
 
                     grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
@@ -37,7 +39,7 @@ namespace Grid_App
                 }
                 change = new Button
                 {
-                    Text = "Change x/0"
+                    Text = "Who's first?"
                 };
                 change.Clicked += Change_Clicked;
                 res = new Button
@@ -46,7 +48,7 @@ namespace Grid_App
                 };
                 res.Clicked += Res_Clicked;
 
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 3; i++) //Генерация поля
                 {
                     for (int j = 0; j < 3; j++)
                     {
@@ -57,6 +59,7 @@ namespace Grid_App
                             BackgroundColor=Color.Green,
                             FontSize = 30,
                             Text = "",
+                            HorizontalTextAlignment = TextAlignment.Center,
                             TextColor=Color.Black,
                             VerticalTextAlignment = TextAlignment.Center,
 
@@ -73,69 +76,82 @@ namespace Grid_App
                 }
 
                 grid.Children.Add(change, 0, 3);
-                grid.Children.Add(res, 3, 3);
+                grid.Children.Add(res, 2,3);
                 Content = grid;
 
             }
-
+      
         private void Res_Clicked(object sender, EventArgs e)
         {
             Reset();
+            chck = 0;//Перезапуск игры
         }
           
-        List<Label> lst = new List<Label>() { };
+        List<Label> lst = new List<Label>() { }; //сохраняем здесь поля
         private void Tap_Tapped(object sender, EventArgs e)
 
             {
 
-            check();
+         
               Label stat = sender as Label;
 
-            if (chck % 2 == 0)
-            {
 
-              stat.Text = "X";
-              chck++;
-              lst.Add(stat);
-             }
-              else if (chck % 2 != 0)
-             {
-              chck++;
-              stat.Text = "0";
-              lst.Add(stat);
-             }
-            else if(stp==true)
-            {
-                DisplayAlert("Alert", "Yes", "ok");
-            }
+
+            if (stat.Text == "") //Проверка на наличие текста в поле
+
+
+                if (chck % 2 == 0)
+                {
+                    change.Text = "0";
+                    stat.Text = "X";
+                    chck++;
+                    lst.Add(stat);
+                
+                }
+                  else if (chck % 2 != 0)
+                 {
+                    change.Text = "X";
+                    chck++;
+                    stat.Text = "0";
+                    lst.Add(stat);
+                 }
+
+                if (checkDraw() == true)
+                {
+                    DisplayAlert("Игра окончена", "Ничья", "Новая игра");
+                    Reset();
+                    chck = 0;
+                }
+
+
 
            }
-        string l = "X";
-        string h = "0";
-        bool stp;
-        void check()
+        bool checkDraw() //Проверка на ничью
         {
-            foreach(Label stat in lst)
+            if(chck==9)
             {
-                if (lst.Contains(stat))
-                {
-                    stp = true;
-                }
-                else
-                {
-                    stp = false;
-                }
-                    
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
+/*        bool checkWinner()
+        {
+
+        }*/
+
         Random strt = new Random();
-
-
-            int chck = 0;
-            private void Change_Clicked(object sender, EventArgs e)
-            {
+        int chck = 0;
+        private void Change_Clicked(object sender, EventArgs e) //Рандомайзер, кто будет ходить первый
+        {
             chck = strt.Next(0, 2);
-            }
+
+    
+        }
+
+
         }
     }
 
